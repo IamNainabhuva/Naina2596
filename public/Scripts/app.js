2a011a7f27cb03458a1c855b183eb05bbf6d31d5
IAmAagamShah <shah.aagam126@gmail.com>
2022-10-06 13:37:50 -0400

    Assignment-1

---
+++ public/Scripts/app.js
@@ -0,0 +1,18 @@
+/**
+  * app.css
+  * Soham Rajput
+  * 301180583
+  * Oct 3, 2021
+  */
+
+// IIFE - Immediately Invoked Function Expression
+(function(){
+
+    function Start()
+    {
+        console.log("App Started...");
+    }
+
+    window.addEventListener("load", Start);
+
+})();
